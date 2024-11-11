3ee506ebaa377f5eb3662dc2a9b0b16e522ab951
Jackie Lin <JLin226859643@gmail.com>
2024-11-10 20:14:10 -0500

    Portfolio Website

--- script.js
+++ script.js
@@ -4,7 +4,35 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
-const toggleDarkMode = () =>{
-    document.body.classList.toggle("dark-mode");
+let animation = {
+    revealDistance: 100,  
+    initialOpacity: 0,
+    transitionDuration: '1s',
+    transitionProperty: 'all',
+    transitionTimingFunction: 'ease',
+    transitionDelay: '0s',
+};
+
+const revealableContainers = document.querySelectorAll('.revealable');
+
+function reveal() {
+    for (let i = 0; i < revealableContainers.length; i++) {
+        let windowHeight = window.innerHeight;
+        let topOfRevealableContainer = revealableContainers[i].getBoundingClientRect().top;
+
+        console.log(`Element ${i}: top=${topOfRevealableContainer}, windowHeight=${windowHeight}`);
+
+        if (topOfRevealableContainer < windowHeight - animation.revealDistance) {
+            revealableContainers[i].classList.add('active');
+        } else {
+            revealableContainers[i].classList.remove('active');
+        }
+    }
 }
-themeButton.addEventListener("click",toggleDarkMode);
+
+window.addEventListener('scroll', reveal);
+reveal();  
+
+
+
+
